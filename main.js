bf195b4bc8688cbb0efc0734103853ea3dec3187
FrancoRodao <francorodao802@gmail.com>
2021-02-19 21:41:45 -0300

    rediseñado por completo: mejor optimizado, cambios en el sistema slide, convertido a una clase con sus metodos y propiedades, re-diseño para en el futuro hacer una libreria

--- main.js
+++ main.js
@@ -1,141 +1,112 @@
 let imagesCarrousel = ['./assets/1.jpg', './assets/2.jpg', './assets/3.jpg', './assets/4.jpg', './assets/5.jpg']
-const carrouselContainer = document.getElementsByClassName('container__imgs')[0]
-
-/* 
-DELETE BLANK SPACES IN CONTAINER (Whitespace inside elements is considered as text, 
-and text is considered as nodes, this could break carrousel) 
-*/
-
-if (carrouselContainer.lastChild) {
-    carrouselContainer.textContent = "";
-}
+const carrouselContainer = document.getElementsByClassName('container')[0]
 
 
-//INSERT ALL IMAGES IN CAROUSEL CONTAINER
-for (let index = 0; index < imagesCarrousel.length; index++) {
-    const image = imagesCarrousel[index];
-    const img = document.createElement('img')
-    img.classList.add('container__img')
-    img.src = image
-    carrouselContainer.appendChild(img)
-}
 
+class Carrousel {
 
-const childNodesImagesContainer = carrouselContainer.childNodes
-let imgIndex = 0
+    container;
+    itemsContainer;
+    images = [];
+    spanIndex;
+    imgIndex = 1;
 
-const spanIndex = document.createElement('span')
-spanIndex.classList.add('container__number')
-spanIndex.textContent = `${imgIndex+1} / ${imagesCarrousel.length}`
-carrouselContainer.appendChild(spanIndex)
+    autoplay;
 
-//CARROUSEL NEXT
-document.getElementById('carrousel_next').addEventListener('click', (e) => {
-    e.preventDefault()
-    imgIndex++
-    const xd = document.getElementsByClassName("container")[0]
-    if (imgIndex == imagesCarrousel.length) {
-        imgIndex = 0
-        xd.scrollLeft -= xd.offsetWidth * imagesCarrousel.length
-        return
-    }
-    xd.scrollLeft += xd.offsetWidth
+    slideDuration = 1000
+    slideing = false
 
-})
-//CARROUSEL BACK
-document.getElementById('carrousel_back').addEventListener('click', (e) => {
-    e.preventDefault()
-    imgIndex--
-    const xd = document.getElementsByClassName("container")[0]
-    if (imgIndex == -1) {
-        console.log('xd')
-        imgIndex = imagesCarrousel.length-1
-        xd.scrollLeft += xd.offsetWidth * imagesCarrousel.length
-        return
-    }
-    xd.scrollLeft -= xd.offsetWidth
-
-})
-
-
-/* class Carrousel{
-
-    container; 
-    images;
-    imgIndex = 0;
-    childNodesImagesContainer = this.container.childNodes
-
-    constructor(container, images){
-        this.container = container
-        this.images = images
-
-        //CREATE HTML
-        
-        
+    constructor(options) {
+        this.container = document.getElementsByClassName("carrousel__container")[0]
+        this.itemsContainer = document.getElementsByClassName("carrousel__items")[0]
+        //get imgs from carrousel items and remove #text's childs
+        document.querySelectorAll(".carrousel__item").forEach(item => {
+            item.childNodes.forEach(child => child.nodeName === "IMG" ? this.images.push(child) : false )
+        })
+        this.setup()
     }
 
-    next(){
-        this.imgIndex++
-        
+    setup(){
+
+        //insert all images in carrouselContainer
+        for (let index = 0; index < this.images.length; index++) {
+            const image = this.images[index];
+            image.classList.add('carrousel__itemImg')  /* img */
+        }
+
+        // create span index image counter
+        this.spanIndex = document.createElement('span')
+        this.spanIndex.classList.add('container__number') //imgIndex
+        this.updateIndex()
+        this.container.appendChild(this.spanIndex)
+
+        //create next prev arrows
+        const arrowNext = document.createElement("span")
+        arrowNext.id = "carrousel_next"
+        arrowNext.textContent = "=>"
+        this.container.appendChild(arrowNext)
+
+        const arrowPrev = document.createElement("span")
+        arrowPrev.id = "carrousel_prev"
+        arrowPrev.textContent = "<="
+        this.container.appendChild(arrowPrev)
+
+        arrowNext.addEventListener("click", ()=>this.next())
+        arrowPrev.addEventListener("click", ()=>this.prev())
+
+    }
+
+    next() {
+        if(this.slideing) return
+        this.slideing = true
+        setTimeout(() => {
+            this.slideing = false
+        }, this.slideDuration);
+
         if (this.imgIndex == this.images.length) {
-            this.imgIndex = 0
+            this.itemsContainer.style.transform= "translateX(0px)"
+            this.imgIndex = 1
+            this.updateIndex()
+            return
         }
-    
-        for (let index = 0; index < this.childNodesImagesContainer.length; index++) {
-            const img = this.childNodesImagesContainer[index];
-    
-            if(img.nodeName === "SPAN"){
-                return
-            }
-    
-            if (index == this.imgIndex) {
-                if (img.classList.contains('none')) {
-                    img.classList.remove('none')
-                    setTimeout(function () {
-                        img.classList.remove('visuallyhidden')
-                    }, 20)
-                }else{
-                    img.classList.add('visuallyhidden')
-                }
-                continue
-            }
-            img.classList.add('visuallyhidden')
-            img.classList.add('none')
-        }
+
+        this.itemsContainer.style.transition = `transform ${this.slideDuration}ms`
+        this.itemsContainer.style.transform = `translateX(-${this.container.clientWidth * this.imgIndex}px)`
+        this.imgIndex++
+        this.updateIndex()
     }
 
-    prev(){
+    prev() {
+        if(this.slideing) return
+        this.slideing = true
+        setTimeout(() => {
+            this.slideing = false
+        }, this.slideDuration);
+
+
+        if (this.imgIndex == 1) {
+            this.itemsContainer.style.transition = `transform ${this.slideDuration}ms`
+            this.itemsContainer.style.transform = `translateX(-${this.container.clientWidth * (this.images.length-1)}px)`
+            this.imgIndex = this.images.length
+            this.updateIndex()
+            return
+        }
+
+        this.itemsContainer.style.transition = `transform ${this.slideDuration}ms`
+        console.log(this.imgIndex)
+        this.itemsContainer.style.transform = `translateX(-${this.container.clientWidth * (this.imgIndex-2)}px)`
         this.imgIndex--
-
-        if (this.imgIndex == -1) {
-            this.imgIndex = this.images.length-1 
-        }
-    
-    
-        for (let index = 0; index < this.childNodesImagesContainer.length; index++) {
-            const img = this.childNodesImagesContainer[index];
-        
-            if(img.nodeName === "SPAN"){
-                return
-            }
-    
-            if (index == this.imgIndex) {
-                if (img.classList.contains('none')) {
-                    img.classList.remove('none')
-                    setTimeout(function () {
-                        img.classList.remove('visuallyhidden')
-                    }, 20)
-                }else{
-                    img.classList.add('visuallyhidden')
-                }
-                continue
-            }
-            img.classList.add('visuallyhidden')
-            img.classList.add('none')
-        }
+        this.updateIndex()
     }
 
+    updateIndex(){
+        this.spanIndex.textContent = `${this.imgIndex} / ${this.images.length}`
+    }
+
+    autoPlay(){
+
+    }
 }
 
-new Carrousel()
- */
+const carrousel = new Carrousel(carrouselContainer, imagesCarrousel)
+
